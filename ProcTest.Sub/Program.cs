328e1fcd83439ec83269c34ef9b45e38f284595a
Tony Valenti <Tony Valenti@DESKTOP-N5CFP67>
2020-10-08 06:16:05 -0500

    Add project files.

---
+++ ProcTest.Sub/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProcTest.Sub
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Sub App");
+            Console.WriteLine("Press Enter to Exit");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ProcTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+
+namespace ProcTest
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main App");
+
+            var EntryPoint = typeof(ProcTest.Sub.Program).Assembly.Location;
+            {
+                var find = ".dll";
+                var replace = ".exe";
+
+                if (EntryPoint.EndsWith(find, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    EntryPoint = EntryPoint[..^replace.Length] + replace;
+                }
+
+            }
+
+            
+
+
+            var PSI = new System.Diagnostics.ProcessStartInfo()
+            {
+                FileName = EntryPoint,
+                UseShellExecute = true,
+            };
+
+            var Proc = System.Diagnostics.Process.Start(PSI);
+
+            Console.WriteLine("Waiting on Process to Exit");
+            Proc.WaitForExit();
+            Console.WriteLine("Exited");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
